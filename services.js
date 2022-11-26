5107942511557ed6f971db9650b191ed064b2ae7
MrsEzaia <mrs.ezaia@mail.ru>
2022-11-26 18:19:54 +0300

    1

---
+++ services.js
@@ -0,0 +1,30 @@
+if (document.documentElement.clientWidth < 768) {
+    const swiper = new Swiper('#swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        spaceBetween: 16,
+        slidesPerView: 1.3,
+
+    });
+}
+
+if (document.documentElement.clientWidth > 768) {
+    const swiper = document.querySelector("#swiper");
+    const btn = document.querySelector(".content__btn-more");
+    btn.onclick = function() {
+        if (swiper.className == "swiper-visible") {
+            swiper.className = "";
+            btn.innerHTML = "Показать все";
+        } else {
+            swiper.className = "swiper-visible";
+            btn.innerHTML = "Скрыть";
+              }
+        }
+    }
